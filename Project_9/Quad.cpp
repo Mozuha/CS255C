ae1d4adbb73e3ef3b71e9b610f30b0d0da492d5f
Mozuha <mizuki.mikkun.hashimoto@gmail.com>
2020-04-20 12:59:33 +0900

    changed get to print

--- Project_9/Quad.cpp
+++ Project_9/Quad.cpp
@@ -22,8 +22,9 @@ Quad::Quad(Point p1, Point p2, Point p3, Point p4) {
 // print coordinates in (x, y)(x, y)(x, y)(x, y) form
 void Quad::print() {
   cout << "Quadrilateral's points:" << endl;
-  cout << "(" << p1.getX() << ", " << p1.getY() << ")";
-  cout << "(" << p2.getX() << ", " << p2.getY() << ")";
-  cout << "(" << p3.getX() << ", " << p3.getY() << ")";
-  cout << "(" << p4.getX() << ", " << p4.getY() << ")" << endl;
+  p1.print();
+  p2.print();
+  p3.print();
+  p4.print();
+  cout << endl;
 }
